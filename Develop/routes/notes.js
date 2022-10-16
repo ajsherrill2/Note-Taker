e9552ace2be4f33ff065db7c2ce2573ccc882ed3
GH0ST <adamjamessherrill@gmail.com>
2022-10-16 11:05:26 -0500

    created end points for notes.js router

---
+++ Develop/routes/notes.js
@@ -0,0 +1,30 @@
+const notes = require("express").Router();
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+const uuid = require("../helpers/uuid");
+
+// GET Route for retreiving all tips in db
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new note
+notes.post('/', (req,res) => {
+    console.log(req.body);
+
+    const {title, text} = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json('Note added successfully');
+    } else {
+        res.errored('Error in adding tip');
+    }
+});
+
+module.exports = notes;
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const api = require("./routes/index");
+const api = require("./routes/index.js");
 
 const PORT = process.env.PORT || 8080;
 
@@ -8,18 +8,18 @@ const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extenden: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
 
 app.use(express.static("public"));
 
 // GET Route for homepage
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname), "/public/index.html")
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname), "./public/index.html")
 );
 
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
 app.listen(PORT, () =>
